11de7ce4a606476acfd922bcf05557b3fceb71fd
huy.nn <huy.nn@algo.com.vn>
2025-05-08 22:09:32 +0700

    .

--- auth-service/internal/usecase/user/user_usecase.go
+++ auth-service/internal/usecase/user/user_usecase.go
@@ -4,7 +4,6 @@ import (
 	"auth_service/internal/config"
 	userdto "auth_service/internal/dtos/user"
 	"auth_service/internal/entities"
-	"auth_service/internal/eventbus/events"
 	"auth_service/internal/infra"
 	"auth_service/internal/repository"
 	"auth_service/pkg"
@@ -284,15 +283,15 @@ func (s *userService) SignUp(ctx context.Context, req userdto.SignUpCommand) (*u
 	}
 
 	fmt.Printf("OTP for %s: %s\n", req.Email, otp)
-	err = s.Cabin.GetInfra().GetEventbus().Publish(ctx, &events.IntegrationEventSendMail{
-		Body:    "OTP của bạn là " + otp,
-		Subject: "Mã xác nhận đăng ký",
-		To:      []string{req.Email},
-	})
-	if err != nil {
-		res.StatusMessage.Code = statusmsg.StatusCode_STATUS_CODE_UNSPECIFIED
-		return res, err
-	}
+	// err = s.Cabin.GetInfra().GetEventbus().Publish(ctx, &events.IntegrationEventSendMail{
+	// 	Body:    "OTP của bạn là " + otp,
+	// 	Subject: "Mã xác nhận đăng ký",
+	// 	To:      []string{req.Email},
+	// })
+	// if err != nil {
+	// 	res.StatusMessage.Code = statusmsg.StatusCode_STATUS_CODE_UNSPECIFIED
+	// 	return res, err
+	// }
 
 	res.StatusMessage.Code = statusmsg.StatusCode_STATUS_CODE_SUCCESS
 	return res, nil
